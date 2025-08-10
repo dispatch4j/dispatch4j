ab5ea18de7d6972341f813e9467e28b7ad327111
Mateusz Filipowicz <matfilipowicz@roche.com>
2025-08-10 23:01:01 +0200

    feat: add Spring Boot autoconfiguration to support spring boot framework

---
+++ dispatch4j-spring-boot-autoconfigure/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("io.spring.dependency-management")
+}
+
+dependencies {
+    api(project(":dispatch4j-core"))
+    
+    implementation("org.springframework.boot:spring-boot-autoconfigure")
+    implementation("org.springframework:spring-context")
+    
+    // Optional dependencies
+    compileOnly("org.springframework.boot:spring-boot-configuration-processor")
+    compileOnly("org.springframework.security:spring-security-core")
+    
+    // Test dependencies
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-core")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.boot:spring-boot-dependencies:3.4.1")
+    }
+}
