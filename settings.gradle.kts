6ed7e32b7b93077ef47309afc499d5de729cc074
Mateusz Filipowicz <matfilipowicz@roche.com>
2025-08-10 22:55:47 +0200

    add initial configuration

---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "dispatch4j"
+
+include("dispatch4j-core")
+include("dispatch4j-standalone")
+include("dispatch4j-spring-boot-starter")
+include("dispatch4j-spring-boot-autoconfigure")
+include("dispatch4j-examples")
