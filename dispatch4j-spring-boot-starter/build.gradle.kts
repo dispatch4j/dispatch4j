71b96cd9f1f24d93f28f32ef2853fa387086036f
Mateusz Filipowicz <matfilipowicz@roche.com>
2025-08-10 23:01:11 +0200

    feat: add Spring Boot Starter

---
+++ dispatch4j-spring-boot-starter/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("io.spring.dependency-management")
+}
+
+dependencies {
+    api(project(":dispatch4j-spring-boot-autoconfigure"))
+    api(project(":dispatch4j-core"))
+    
+    // Spring Boot Starter dependencies
+    api("org.springframework.boot:spring-boot-starter")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.boot:spring-boot-dependencies:3.4.1")
+    }
+}
